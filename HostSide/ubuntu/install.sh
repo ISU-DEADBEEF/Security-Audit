a47ddfd3d4af51c89bcf37da163a42c80dc7fa5f
nathancarterevans <natec.carter15@gmail.com>
2024-07-05 21:56:27 -0500

    Clean slate protocol

--- HostSide/ubuntu/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /usr/local/bin/audit
-mkdir -p /etc/init.d/audit
-
-cp startConnection /etc/init.d/audit/startConnection
-cp file-sender.py /usr/local/bin/audit/file-sender.py
-
-chmod +x /etc/init.d/audit /usr/local/bin/audit/file-sender.py
